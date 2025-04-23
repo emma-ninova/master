96fa8f7a30871e0f5f39f16114573e2d17b28e6a
Emanuela Ninova <emma-ninova@users.noreply.github.com>
2025-04-23 15:25:38 +0300

    Deploy production build to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/master/', // Add this line to specify the base path for GitHub Pages
 })
